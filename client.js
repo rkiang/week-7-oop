6347045672dd73e9a6cc751316eb55ce4d45887d
rkiang <ryanlkiang@gmail.com>
2017-09-05 20:43:06 -0500

    added sleep class

--- client.js
+++ client.js
@@ -1,1 +1,17 @@
 console.log('client.js is sourced in');
+
+class Sleep {
+    constructor(hours, time) {
+        this.hours = hours; // the amount of hours a person sleeps
+        this.time = 24; // hours in a day
+
+        if (hours = time) {
+            console.log('Too many hours slept');
+
+        } else {
+            console.log('May sleep more in a day');
+        }
+    }
+}
+let Weekend = new Sleep(8)
+console.log(Weekend);
